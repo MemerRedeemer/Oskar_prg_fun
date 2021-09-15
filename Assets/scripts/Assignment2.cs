64b554f549083454de63dc41a019ab33cc2917ca
Muppet237 <42935807+MemerRedeemer@users.noreply.github.com>
2021-09-15 10:46:50 +0200

    Added Modulus Operator
    Added Modulus operator in assignment 2. Now changes color every third line.

--- Assets/scripts/Assignment2.cs
+++ Assets/scripts/Assignment2.cs
@@ -6,6 +6,7 @@ public class Assignment2 : ProcessingLite.GP21 {
 
     float startPointX = 1;
     float endPointY = 19;
+    float checkValue = 3;
 
     void Start() {
         Background(0, 0, 0);
@@ -15,6 +16,12 @@ public class Assignment2 : ProcessingLite.GP21 {
         for(int i = 0; i < endPointY; i++) {
             startPointX += 1;
             endPointY -= 1;
+            if(startPointX % checkValue == 1) {
+                checkValue += 3;
+                Stroke(255, 0, 0);
+            } else {
+                Stroke(0, 0, 255);
+            }
             Line(startPointX, 1, 1, endPointY);
         }
     }
