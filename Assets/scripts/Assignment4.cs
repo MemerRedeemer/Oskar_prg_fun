3b016aa276a0156eec4eb4cde7164a2f3a77b8f9
Muppet237 <42935807+MemerRedeemer@users.noreply.github.com>
2021-09-20 09:54:48 +0200

    Fine tuning
    Assignment 4 fine tuning some variables and such

--- Assets/scripts/Assignment4.cs
+++ Assets/scripts/Assignment4.cs
@@ -6,7 +6,11 @@ public class Assignment4 : ProcessingLite.GP21 {
 
     Vector2 velPos;
     Vector2 acsPos;
-    Vector2 acceleration = new Vector2(0,0); 
+    Vector2 acceleration = new Vector2(0, 0);
+
+    float acsDecSpeed = 0.025f;
+    float acsIncSpeed = 0.1f;
+
     float rad = 3;
     float speed = 5;
 
@@ -27,25 +31,25 @@ public class Assignment4 : ProcessingLite.GP21 {
 
         //Acceleration Circle
         if(Input.GetKey(KeyCode.D)) {
-            acceleration.x += 0.2f * Time.deltaTime;
+            acceleration.x += acsIncSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.A)) {
-            acceleration.x -= 0.2f * Time.deltaTime;
+            acceleration.x -= acsIncSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.S)) {
-            acceleration.y -= 0.2f * Time.deltaTime;
+            acceleration.y -= acsIncSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.W)) {
-            acceleration.y += 0.2f * Time.deltaTime;
+            acceleration.y += acsIncSpeed * Time.deltaTime;
         } else {
             if(acceleration.x >= 0) {
-                acceleration.x -= 0.1f * Time.deltaTime;
+                acceleration.x -= acsDecSpeed * Time.deltaTime;
             }
             if(acceleration.x <= 0) {
-                acceleration.x += 0.1f * Time.deltaTime;
+                acceleration.x += acsDecSpeed * Time.deltaTime;
             }
             if(acceleration.y >= 0) {
-                acceleration.y -= 0.1f * Time.deltaTime;
+                acceleration.y -= acsDecSpeed * Time.deltaTime;
             }
             if(acceleration.y <= 0) {
-                acceleration.y += 0.1f * Time.deltaTime;
+                acceleration.y += acsDecSpeed * Time.deltaTime;
             }
         }
         acsPos.x += acceleration.x;
